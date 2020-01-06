10d73fc2c002138fc254677324321401ce16bfe7
andriuskin111 <andriuskin@gmail.com>
2020-01-06 19:05:50 +0200

    All done

--- ND_WindowsForms_5/ND_WindowsForms_5/Form1.Designer.cs
+++ ND_WindowsForms_5/ND_WindowsForms_5/Form1.Designer.cs
@@ -28,13 +28,68 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.tbInput = new System.Windows.Forms.TextBox();
+            this.btnKonvertuoti = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tbOutput = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // tbInput
+            // 
+            this.tbInput.Location = new System.Drawing.Point(148, 62);
+            this.tbInput.Name = "tbInput";
+            this.tbInput.Size = new System.Drawing.Size(100, 26);
+            this.tbInput.TabIndex = 0;
+            // 
+            // btnKonvertuoti
+            // 
+            this.btnKonvertuoti.Location = new System.Drawing.Point(42, 141);
+            this.btnKonvertuoti.Name = "btnKonvertuoti";
+            this.btnKonvertuoti.Size = new System.Drawing.Size(308, 64);
+            this.btnKonvertuoti.TabIndex = 1;
+            this.btnKonvertuoti.Text = "Konvertuoti";
+            this.btnKonvertuoti.UseVisualStyleBackColor = true;
+            this.btnKonvertuoti.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(38, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(312, 20);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Iveskite temperatura laipsniais pagal celsiju";
+            // 
+            // tbOutput
+            // 
+            this.tbOutput.Location = new System.Drawing.Point(42, 212);
+            this.tbOutput.Multiline = true;
+            this.tbOutput.Name = "tbOutput";
+            this.tbOutput.Size = new System.Drawing.Size(308, 226);
+            this.tbOutput.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(396, 450);
+            this.Controls.Add(this.tbOutput);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnKonvertuoti);
+            this.Controls.Add(this.tbInput);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox tbInput;
+        private System.Windows.Forms.Button btnKonvertuoti;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tbOutput;
     }
 }
 
--- ND_WindowsForms_5/ND_WindowsForms_5/Form1.cs
+++ ND_WindowsForms_5/ND_WindowsForms_5/Form1.cs
@@ -16,5 +16,30 @@ namespace ND_WindowsForms_5
         {
             InitializeComponent();
         }
+
+        double tempC;
+        double tempK;
+        double tempF;
+        double tempR;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(!double.TryParse(tbInput.Text, out tempC))
+            {
+                MessageBox.Show("Ivesti neteisingi duomenys");
+                return;
+            }
+
+            tempC = double.Parse(tbInput.Text);
+            tempK = tempC + 273.16;
+            tempF = tempC * 9 / 5 + 32;
+            tempR = tempC * 0.8;
+
+            tbOutput.Clear();
+            tbOutput.AppendText($"TEMPERATURA\r\n");
+            tbOutput.AppendText($"Celsijais: {tempC}\r\n");
+            tbOutput.AppendText($"Kelvinais: {tempK}\r\n");
+            tbOutput.AppendText($"Fahrenheitais: {tempF}\r\n");
+            tbOutput.AppendText($"Reomiurais: {tempR}\r\n");
+        }
     }
 }
