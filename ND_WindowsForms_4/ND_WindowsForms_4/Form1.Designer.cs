1598c9794dc22e87a4342199e7a6afe8e8dcf20a
andriuskin111 <andriuskin@gmail.com>
2020-01-06 02:00:51 +0200

    All done

--- ND_WindowsForms_4/ND_WindowsForms_4/Form1.Designer.cs
+++ ND_WindowsForms_4/ND_WindowsForms_4/Form1.Designer.cs
@@ -28,13 +28,161 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.tbOutput = new System.Windows.Forms.TextBox();
+            this.btnParodyti = new System.Windows.Forms.Button();
+            this.tbPazimys1 = new System.Windows.Forms.TextBox();
+            this.tbPazimys2 = new System.Windows.Forms.TextBox();
+            this.tbPazimys3 = new System.Windows.Forms.TextBox();
+            this.btnIvestiPazymi1 = new System.Windows.Forms.Button();
+            this.btnIvestiPazymi2 = new System.Windows.Forms.Button();
+            this.btnIvestiPazymi3 = new System.Windows.Forms.Button();
+            this.lblPazSkaic1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(20, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(135, 20);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Pirmas semestras";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(21, 101);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(134, 20);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Antras semestras";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(21, 171);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(138, 20);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Trecias semestras";
+            // 
+            // tbOutput
+            // 
+            this.tbOutput.Location = new System.Drawing.Point(25, 311);
+            this.tbOutput.Multiline = true;
+            this.tbOutput.Name = "tbOutput";
+            this.tbOutput.Size = new System.Drawing.Size(392, 239);
+            this.tbOutput.TabIndex = 6;
+            // 
+            // btnParodyti
+            // 
+            this.btnParodyti.Location = new System.Drawing.Point(25, 223);
+            this.btnParodyti.Name = "btnParodyti";
+            this.btnParodyti.Size = new System.Drawing.Size(392, 64);
+            this.btnParodyti.TabIndex = 7;
+            this.btnParodyti.Text = "Parodyti informacija";
+            this.btnParodyti.UseVisualStyleBackColor = true;
+            this.btnParodyti.Click += new System.EventHandler(this.btnParodyti_Click);
+            // 
+            // tbPazimys1
+            // 
+            this.tbPazimys1.Location = new System.Drawing.Point(171, 24);
+            this.tbPazimys1.Name = "tbPazimys1";
+            this.tbPazimys1.Size = new System.Drawing.Size(78, 26);
+            this.tbPazimys1.TabIndex = 8;
+            // 
+            // tbPazimys2
+            // 
+            this.tbPazimys2.Location = new System.Drawing.Point(171, 98);
+            this.tbPazimys2.Name = "tbPazimys2";
+            this.tbPazimys2.Size = new System.Drawing.Size(78, 26);
+            this.tbPazimys2.TabIndex = 9;
+            // 
+            // tbPazimys3
+            // 
+            this.tbPazimys3.Location = new System.Drawing.Point(171, 168);
+            this.tbPazimys3.Name = "tbPazimys3";
+            this.tbPazimys3.Size = new System.Drawing.Size(78, 26);
+            this.tbPazimys3.TabIndex = 10;
+            // 
+            // btnIvestiPazymi1
+            // 
+            this.btnIvestiPazymi1.Location = new System.Drawing.Point(268, 18);
+            this.btnIvestiPazymi1.Name = "btnIvestiPazymi1";
+            this.btnIvestiPazymi1.Size = new System.Drawing.Size(149, 38);
+            this.btnIvestiPazymi1.TabIndex = 11;
+            this.btnIvestiPazymi1.Text = "Ivesti Pazymi";
+            this.btnIvestiPazymi1.UseVisualStyleBackColor = true;
+            this.btnIvestiPazymi1.Click += new System.EventHandler(this.btnIvestiPazymi1_Click);
+            // 
+            // btnIvestiPazymi2
+            // 
+            this.btnIvestiPazymi2.Location = new System.Drawing.Point(268, 92);
+            this.btnIvestiPazymi2.Name = "btnIvestiPazymi2";
+            this.btnIvestiPazymi2.Size = new System.Drawing.Size(149, 38);
+            this.btnIvestiPazymi2.TabIndex = 12;
+            this.btnIvestiPazymi2.Text = "Ivesti Pazymi";
+            this.btnIvestiPazymi2.UseVisualStyleBackColor = true;
+            this.btnIvestiPazymi2.Click += new System.EventHandler(this.btnIvestiPazymi2_Click);
+            // 
+            // btnIvestiPazymi3
+            // 
+            this.btnIvestiPazymi3.Location = new System.Drawing.Point(268, 162);
+            this.btnIvestiPazymi3.Name = "btnIvestiPazymi3";
+            this.btnIvestiPazymi3.Size = new System.Drawing.Size(149, 38);
+            this.btnIvestiPazymi3.TabIndex = 13;
+            this.btnIvestiPazymi3.Text = "Ivesti Pazymi";
+            this.btnIvestiPazymi3.UseVisualStyleBackColor = true;
+            this.btnIvestiPazymi3.Click += new System.EventHandler(this.btnIvestiPazymi3_Click);
+            // 
+            // lblPazSkaic1
+            // 
+            this.lblPazSkaic1.AutoSize = true;
+            this.lblPazSkaic1.Location = new System.Drawing.Point(59, 51);
+            this.lblPazSkaic1.Name = "lblPazSkaic1";
+            this.lblPazSkaic1.Size = new System.Drawing.Size(0, 20);
+            this.lblPazSkaic1.TabIndex = 14;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(441, 576);
+            this.Controls.Add(this.lblPazSkaic1);
+            this.Controls.Add(this.btnIvestiPazymi3);
+            this.Controls.Add(this.btnIvestiPazymi2);
+            this.Controls.Add(this.btnIvestiPazymi1);
+            this.Controls.Add(this.tbPazimys3);
+            this.Controls.Add(this.tbPazimys2);
+            this.Controls.Add(this.tbPazimys1);
+            this.Controls.Add(this.btnParodyti);
+            this.Controls.Add(this.tbOutput);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Pazymiai";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox tbOutput;
+        private System.Windows.Forms.Button btnParodyti;
+        private System.Windows.Forms.TextBox tbPazimys1;
+        private System.Windows.Forms.TextBox tbPazimys2;
+        private System.Windows.Forms.TextBox tbPazimys3;
+        private System.Windows.Forms.Button btnIvestiPazymi1;
+        private System.Windows.Forms.Button btnIvestiPazymi2;
+        private System.Windows.Forms.Button btnIvestiPazymi3;
+        private System.Windows.Forms.Label lblPazSkaic1;
     }
 }
 
--- ND_WindowsForms_4/ND_WindowsForms_4/Form1.cs
+++ ND_WindowsForms_4/ND_WindowsForms_4/Form1.cs
@@ -16,5 +16,182 @@ namespace ND_WindowsForms_4
         {
             InitializeComponent();
         }
+        List<int> pazymiaiPirmas = new List<int>();
+        List<int> pazymiaiAntras = new List<int>();
+        List<int> pazymiaiTrecias = new List<int>();
+        int pazymys;
+        double vidurkis = 0;
+
+        
+        private void btnIvestiPazymi1_Click(object sender, EventArgs e)
+        {
+            if (tbPazimys1.Text == "")
+            {
+                MessageBox.Show("Langelis turi buti uzpildytas");
+                return;
+            }
+
+            try
+            {
+                if (!int.TryParse((tbPazimys1.Text), out pazymys))
+                {
+                    MessageBox.Show("Ivesti neteisingi duomenys");
+                    tbPazimys1.Text = "";
+                    return;
+                }
+                else if (int.TryParse(tbPazimys1.Text, out pazymys))
+                {
+                    if (pazymys < 0 || pazymys > 10)
+                    {
+                        MessageBox.Show("Ivesti neteisingi duomenys");
+                        tbPazimys1.Text = "";
+                        return;
+                    }
+
+                    pazymiaiPirmas.Add(pazymys);
+
+                    if(pazymiaiPirmas.Count > 0)
+                    {
+                        label1.ForeColor = Color.Green;
+                    }
+                }
+
+                tbPazimys1.Text = "";
+            }
+            catch
+            {
+                MessageBox.Show("Error");
+                return;
+            }
+        }
+
+        private void btnIvestiPazymi2_Click(object sender, EventArgs e)
+        {
+            if (tbPazimys2.Text == "")
+            {
+                MessageBox.Show("Langelis turi buti uzpildytas");
+                return;
+            }
+
+            try
+            {
+                if (!int.TryParse((tbPazimys2.Text), out pazymys))
+                {
+                    MessageBox.Show("Ivesti neteisingi duomenys");
+                    tbPazimys2.Text = "";
+                    return;
+                }
+                else if (int.TryParse(tbPazimys2.Text, out pazymys))
+                {
+                    if (pazymys < 0 || pazymys > 10)
+                    {
+                        MessageBox.Show("Ivesti neteisingi duomenys");
+                        tbPazimys2.Text = "";
+                        return;
+                    }
+
+                    pazymiaiAntras.Add(pazymys);
+
+                    if (pazymiaiPirmas.Count > 0)
+                    {
+                        label2.ForeColor = Color.Green;
+                    }
+                }
+
+                tbPazimys2.Text = "";
+            }
+            catch
+            {
+                MessageBox.Show("Error");
+                return;
+            }
+        }
+
+        private void btnIvestiPazymi3_Click(object sender, EventArgs e)
+        {
+            if (tbPazimys3.Text == "")
+            {
+                MessageBox.Show("Langelis turi buti uzpildytas");
+                return;
+            }
+
+            try
+            {
+                if (!int.TryParse((tbPazimys3.Text), out pazymys))
+                {
+                    MessageBox.Show("Ivesti neteisingi duomenys");
+                    tbPazimys2.Text = "";
+                    return;
+                }
+                else if (int.TryParse(tbPazimys3.Text, out pazymys))
+                {
+                    if (pazymys < 0 || pazymys > 10)
+                    {
+                        MessageBox.Show("Ivesti neteisingi duomenys");
+                        tbPazimys3.Text = "";
+                        return;
+                    }
+
+                    pazymiaiTrecias.Add(pazymys);
+                    if (pazymiaiPirmas.Count > 0)
+                    {
+                        label3.ForeColor = Color.Green;
+                    }
+                }
+
+                tbPazimys3.Text = "";
+            }
+            catch
+            {
+                MessageBox.Show("Error");
+                return;
+            }
+        }
+
+        private void btnParodyti_Click(object sender, EventArgs e)
+        {
+            tbOutput.Clear();
+
+            if (pazymiaiPirmas.Count == 0 || pazymiaiAntras.Count == 0 || pazymiaiTrecias.Count == 0)
+            {
+                MessageBox.Show("Uzpidykite pazymiais visus semestrus");
+                return;
+            }
+
+            tbOutput.AppendText("Pirmo semestro pazymiai:\r\n");
+
+            foreach (var item in pazymiaiPirmas)
+            {
+                tbOutput.AppendText($" {item}");
+            }
+
+            tbOutput.AppendText("\r\n");
+            vidurkis = pazymiaiPirmas.Average();
+            tbOutput.AppendText($"Pirmo semestro vidurkis: {vidurkis}\r\n");
+
+            tbOutput.AppendText("Antro semestro pazymiai:\r\n");
+
+            foreach (var item in pazymiaiAntras)
+            {
+                tbOutput.AppendText($" {item}");
+            }
+
+            tbOutput.AppendText("\r\n");
+            vidurkis = pazymiaiAntras.Average();
+            tbOutput.AppendText($"Antro semestro vidurkis: {vidurkis}\r\n");
+
+            tbOutput.AppendText("Trecio semestro pazymiai:\r\n");
+
+            foreach (var item in pazymiaiTrecias)
+            {
+                tbOutput.AppendText($" {item}");
+            }
+
+            tbOutput.AppendText("\r\n");
+            vidurkis = pazymiaiTrecias.Average();
+            tbOutput.AppendText($"Trecio semestro vidurkis: {vidurkis}\r\n");
+        }
+
     }
+
 }
